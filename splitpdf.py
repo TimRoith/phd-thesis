bc0f4cb48837f614d1ef7302ec329ab4631346ae
Tim Roith <tim.roith@fau.de>
2023-08-20 11:40:29 +0200

    :fleur_de_lis: add conclusion

---
+++ splitpdf.py
@@ -0,0 +1,14 @@
+from PyPDF2 import PdfWriter, PdfReader
+
+inputpdf = PdfReader(open("thesis.pdf", "rb"))
+output = PdfWriter()
+
+splits = {15:'Intro', 61:'PartA', 101:'PartB', 116:'Outro'}
+
+for i in range(len(inputpdf.pages)):
+    output.add_page(inputpdf.pages[i])
+    if i in splits:
+        with open("splits/" + splits[i] + ".pdf", 'wb') as outputStream:
+            output.write(outputStream)
+        
+        output = PdfWriter()
