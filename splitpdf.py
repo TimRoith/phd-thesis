15674957b5d25b2240b134527aca7613f3224b3a
Tim Roith <tim.roith@fau.de>
2023-09-05 15:10:46 +0200

    changes

--- splitpdf.py
+++ splitpdf.py
@@ -3,12 +3,13 @@ from PyPDF2 import PdfWriter, PdfReader
 inputpdf = PdfReader(open("thesis.pdf", "rb"))
 output = PdfWriter()
 
-splits = {15:'Intro', 61:'PartA', 101:'PartB', 116:'Outro'}
+splits = {15:'Intro', 61:'SSL', 101:'SL', 116:'Outro'}
 
 for i in range(len(inputpdf.pages)):
     output.add_page(inputpdf.pages[i])
     if i in splits:
         with open("splits/" + splits[i] + ".pdf", 'wb') as outputStream:
             output.write(outputStream)
+            print(splits[i])
         
         output = PdfWriter()
